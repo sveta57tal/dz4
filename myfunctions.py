7b6e48b377c482e30dfde2b3a246696aea214489
sveta57tal <stalaeva57@gmail.com>
2023-06-14 13:45:43 +0300

    Initial Commit

---
+++ myfunctions.py
@@ -0,0 +1,167 @@
+"""
+МОДУЛЬ 1
+В модуле прописаны заготовки для 8 функций
+Под каждой функцией есть описание как она должна работать
+ниже есть примеры использования функции
+Задание: реализовать код функции, чтобы он работал по описанию и примеры использования давали верный результат
+"""
+
+# 1
+def simple_separator():
+    return '**********'
+
+
+print(simple_separator() == '**********')  # True
+
+# 2
+def long_separator(count):
+    """
+    Функция создает разделитель из звездочек число которых можно регулировать параметром count
+    :param count: количество звездочек
+    :return: строка разделитель, примеры использования ниже
+    """
+    return '*' * count
+
+
+print(long_separator(3) == '***')  # True
+print(long_separator(4) == '****')  # True
+
+# 3
+def separator(simbol, count):
+    """
+    Функция создает разделитель из любых символов любого количества
+    :param simbol: символ разделителя
+    :param count: количество повторений
+    :return: строка разделитель примеры использования ниже
+    """
+    return simbol * count
+
+
+print(separator('-', 10) == '----------')  # True
+print(separator('#', 5) == '#####')  # True
+
+# 4
+def hello_world():
+    """
+    Функция печатает Hello World в формате:
+    **********
+
+    Hello World!
+
+    ##########
+    :return: None
+    """
+    print("************\n\nпривет, мир!\n\n##########")
+
+
+
+'''
+**********
+
+Hello World!
+
+##########
+'''
+
+hello_world()
+
+#5
+def hello_who(who='World'):
+    """
+    Функция печатает приветствие в красивом формате
+    **********
+
+    Hello {who}!
+
+    ##########
+    :param who: кого мы приветствуем, по умолчанию World
+    :return: None
+    """
+    print("**********\n\nHello {}!\n\n##########".format(who))
+
+
+'''
+**********
+
+Hello World!
+
+##########
+'''
+hello_who()
+'''
+**********
+
+Hello Max!
+
+##########
+'''
+hello_who('Max')
+'''
+**********
+
+Hello Kate!
+
+##########
+'''
+hello_who('Kate')
+
+#6
+def pow_many(power, *args):
+    """
+    Функция складывает любое количество цифр и возводит результат в степень power (примеры использования ниже)
+    :param power: степень
+    :param args: любое количество цифр
+    :return: результат вычисления # True -> (1 + 2)**1
+    """
+    result = sum(args)
+    return result ** power
+
+
+print(pow_many(1, 1, 2) == 3)  # True -> (1 + 2)**1 == 3
+print(pow_many(1, 2, 3) == 5)  # True -> (2 + 3)**1 == 5
+print(pow_many(2, 1, 1) == 4)  # True -> (1 + 1)**2 == 4
+print(pow_many(3, 2) == 8)  # True -> 2**3 == 8
+print(pow_many(2, 1, 2, 3, 4) == 100)  # True -> (1 + 2 + 3 + 4)**2 == 10**2 == 100
+
+#7
+def print_key_val(**kwargs):
+    """
+    Функция выводит переданные параметры в фиде key --> value
+    key - имя параметра
+    value - значение параметра
+    :param kwargs: любое количество именованных параметров
+    :return: None
+    """
+    for k, v in kwargs.items():
+        print(k, '-->', v)
+
+
+"""
+name --> Max
+age --> 21
+"""
+print_key_val(name='Max', age=21)
+"""
+animal --> Cat
+is_animal --> True
+"""
+print_key_val(animal='Cat', is_animal=True)
+
+# 8
+def my_filter(iterable, function):
+    """
+    (Усложненое задание со *)
+    Функция фильтрует последовательность iterable и возвращает новую
+    Если function от элемента последовательности возвращает True, то элемент входит в новую последовательность иначе нет
+    (примеры ниже)
+    :param iterable: входаня последовательности
+    :param function: функция фильтрации
+    :return: новая отфильтрованная последовательность
+    """
+    return [x for x in iterable if function(x)]
+
+
+print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x != 3) == [1, 2, 4, 5])  # True
+print(my_filter(['a', 'b', 'c', 'd'], lambda x: x in 'abba') == ['a', 'b'])  # True
